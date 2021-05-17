1aff37ef5d933b134f3e33049e50f405c15d2901
Бозулан Камчиев <bozulan.krut@gmail.com>
2021-05-17 18:26:33 +0300

    Добавьте файлы проекта.

---
+++ igra/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿
+namespace igra
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 5;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1262, 673);
+            this.MaximumSize = new System.Drawing.Size(1280, 720);
+            this.Name = "Form1";
+            this.Text = "igra";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ igra/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace igra
+{
+    public partial class Form1 : Form
+    {
+        PictureBox[] cloud;
+        int backgroudspeed;
+        Random random;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            for (int i=0; i < cloud.Length; i++)
+            {
+                cloud[i].Left += backgroudspeed;
+                if(cloud[i].Left>=1280)
+                {
+                    cloud[i].Left = cloud[i].Height;
+                }
+            }
+
+            for(int i=0; i<cloud.Length; i++)
+            {
+              cloud[i].Left += backgroudspeed - 10;
+
+              if (cloud[i].Left>=1280)
+                {
+                    cloud[i].Left = cloud[i].Left;
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            backgroudspeed = 10;
+            cloud = new PictureBox[20];
+            random = new Random();
+
+            for (int i = 0; i < cloud.Length; i++)
+                {
+                cloud[i] = new PictureBox();
+                cloud[i].BorderStyle = BorderStyle.None;
+                cloud[i].Location = new Point(random.Next(-1000, 1280), random.Next(140, 320));
+                if (i % 2 == 1)
+                {
+                    cloud[i].Size = new Size(random.Next(100, 225), random.Next(30, 70));
+                    cloud[i].BackColor = Color.FromArgb(random.Next(50, 125), 255, 200, 200);
+                }
+                else
+                {
+                    cloud[i].Size = new Size(150, 25);
+                    cloud[i].BackColor = Color.FromArgb(random.Next(50, 125), 255, 205, 205);
+                }
+                this.Controls.Add(cloud[i]);
+            }
+        }
+    }
+}
